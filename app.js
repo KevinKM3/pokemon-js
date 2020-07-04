f587e1de903d9de7e1800cb7b372d57ad6a2bab0
Kevin Matthews <kevmatthews3@gmail.com>
2020-07-03 21:34:15 -0400

    dom-objects

---
+++ app.js
@@ -0,0 +1,22 @@
+// DOM Objects
+
+const mainScreen = document.querySelector(".main-screen");
+const pokeName = document.querySelector(".poke-name");
+const pokeId = document.querySelector(".poke-id");
+const pokeFrontImage = document.querySelector(".poke-front-image");
+const pokeBackImage = document.querySelector(".poke-back-image");
+const pokeTypeOne = document.querySelector(".poke-type-one");
+const pokeTypeTwo = document.querySelector(".poke-type-two");
+const pokeWeight = document.querySelector(".poke-weight");
+const pokeHeight = document.querySelector(".poke-height");
+
+fetch("https://pokeapi.co/api/v2/pokemon/1")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    mainScreen.classList.remove("hide");
+    pokeName.textContent = data["name"];
+    pokeId.textContent = data["id"];
+    pokeWeight.textContent = data["weight"];
+    pokeHeight.textContent = data["height"];
+  });
